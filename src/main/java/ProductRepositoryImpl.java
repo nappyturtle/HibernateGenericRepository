5bb101f92aafac27c2494576d96bb7e20c695543
Vo Gia Vu <vuvgse62267@fpt.edu.vn>
2019-02-13 10:55:06 +0700

    3

--- src/main/java/ProductRepositoryImpl.java
+++ src/main/java/ProductRepositoryImpl.java
@@ -12,7 +12,7 @@ public class ProductRepositoryImpl extends GenericRepositoryImpl<Product> {
     }
 
     public void updateName(Product product) {
-        updateProperties(product, "name1");
+        updateProperties(product, "name3");
     }
 
     public void updateCategory(Product product) {
